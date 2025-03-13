aa083ab962d911a57d8484e7f30d2bc2472bdca4
merge-simpson <letsdev@kakao.com>
2025-03-13 09:00:45 +0900

    initial commit

---
+++ src/main/java/nettee/DemoMultimoduleApplication.java
@@ -0,0 +1,13 @@
+package nettee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMultimoduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMultimoduleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nettee/DemoMultimoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package nettee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMultimoduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
