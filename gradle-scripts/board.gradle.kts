3347e2169d43e73bc7c0f21034d4a96844d9b2ed
merge-simpson <letsdev@kakao.com>
2025-03-16 16:26:01 +0900

    feat(board): add rdb adapter module

--- gradle-scripts/board.gradle.kts
+++ gradle-scripts/board.gradle.kts
@@ -12,6 +12,7 @@ include(
 // application layer, adapter modules
 include(
     ":board:application",
+    ":board:rdb-adapter",
 )
 
 project(":board").projectDir = file("${rootProject.projectDir}/services/board")
@@ -20,3 +21,4 @@ project(":board:domain").projectDir = file("${rootProject.projectDir}/services/b
 project(":board:readmodel").projectDir = file("${rootProject.projectDir}/services/board/api/readmodel")
 project(":board:exception").projectDir = file("${rootProject.projectDir}/services/board/api/exception")
 project(":board:application").projectDir = file("${rootProject.projectDir}/services/board/application")
+project(":board:rdb-adapter").projectDir = file("${rootProject.projectDir}/services/board/driven/rdb")
