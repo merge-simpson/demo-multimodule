fd20444af4ec539b6a60fc21a078db7485d6d6dc
merge-simpson <letsdev@kakao.com>
2025-03-16 16:24:46 +0900

    feat(board): add api modules (api, domain, read model, exception)

--- gradle-scripts/board.gradle.kts
+++ gradle-scripts/board.gradle.kts
@@ -1,2 +1,16 @@
+// bundler module
 include(":board")
+
+// API modules
+include(
+    ":board:api",
+    ":board:domain",
+    ":board:readmodel",
+    ":board:exception",
+)
+
 project(":board").projectDir = file("${rootProject.projectDir}/services/board")
+project(":board:api").projectDir = file("${rootProject.projectDir}/services/board/api")
+project(":board:domain").projectDir = file("${rootProject.projectDir}/services/board/api/domain")
+project(":board:readmodel").projectDir = file("${rootProject.projectDir}/services/board/api/readmodel")
+project(":board:exception").projectDir = file("${rootProject.projectDir}/services/board/api/exception")
