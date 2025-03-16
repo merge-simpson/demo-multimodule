03d1345092d112d6b0c64249677a8ce759f0db5c
merge-simpson <letsdev@kakao.com>
2025-03-16 15:29:53 +0900

    build(jpa): add spring-boot-autoconfigure to use `@EntityScan`

--- core-modules/nettee-jpa-core/build.gradle.kts
+++ core-modules/nettee-jpa-core/build.gradle.kts
@@ -14,6 +14,9 @@ dependencies {
     compileOnly("jakarta.persistence:jakarta.persistence-api")
     compileOnly("org.springframework.data:spring-data-commons")
     compileOnly("org.springframework.data:spring-data-jpa")
+    compileOnly("org.springframework.boot:spring-boot-autoconfigure")
+
+    // lombok
     compileOnly("org.projectlombok:lombok")
 }
 
