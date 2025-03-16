24a942970e4a388f9144d7475afc65ccb67d06c1
merge-simpson <letsdev@kakao.com>
2025-03-16 10:54:22 +0900

    build(gradle): divide the gradle script files of common module and core modules from settings.gradle.kts

---
+++ gradle-scripts/core.gradle.kts
@@ -0,0 +1,14 @@
+val coreDir = "../core-modules"
+
+include(
+    ":nettee:jpa-core",
+    ":nettee:cors-webmvc",
+    ":nettee:cors-api",
+    ":nettee:exception-handler-webmvc",
+)
+
+project(":nettee:jpa-core").projectDir = file("$coreDir/nettee-jpa-core")
+project(":nettee:cors-webmvc").projectDir = file("$coreDir/cors/nettee-cors-webmvc")
+project(":nettee:cors-api").projectDir = file("$coreDir/cors/nettee-cors-api")
+project(":nettee:exception-handler-webmvc").projectDir =
+    file("$coreDir/exception-handler/nettee-global-exception-handler-webmvc")
