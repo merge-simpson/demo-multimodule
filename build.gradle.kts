707e5109dbaffe3e5cf5dd344db1b82747d6c93f
merge-simpson <letsdev@kakao.com>
2025-03-16 12:20:55 +0900

    build(gradle): apply version and group name to all projects

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,10 @@ plugins {
     id("io.spring.dependency-management") version "1.1.7"
 }
 
-group = "nettee"
-version = "0.0.1-SNAPSHOT"
+allprojects {
+    group = "nettee"
+    version = "0.0.1-SNAPSHOT"
+}
 
 java {
     toolchain {
