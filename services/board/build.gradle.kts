fbd33e2f3756d1941782df43dbd219c1d29df263
merge-simpson <letsdev@kakao.com>
2025-03-16 16:11:04 +0900

    feat(board): create board directory as a module

---
+++ services/board/build.gradle.kts
@@ -0,0 +1,10 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+dependencies {
+    // api(project("..."))
+}
+
+// NOTE root 프로젝트에서 false 기본값을 주면 이 부분은 생략해도 됨.
+tasks.withType<BootJar> {
+    enabled = false
+}
