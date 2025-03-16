47da3f20be594ecc01ec12b4b0fc0669d257fc0e
merge-simpson <letsdev@kakao.com>
2025-03-16 16:25:18 +0900

    feat(board): add application module

--- gradle-scripts/board.gradle.kts
+++ gradle-scripts/board.gradle.kts
@@ -9,8 +9,14 @@ include(
     ":board:exception",
 )
 
+// application layer, adapter modules
+include(
+    ":board:application",
+)
+
 project(":board").projectDir = file("${rootProject.projectDir}/services/board")
 project(":board:api").projectDir = file("${rootProject.projectDir}/services/board/api")
 project(":board:domain").projectDir = file("${rootProject.projectDir}/services/board/api/domain")
 project(":board:readmodel").projectDir = file("${rootProject.projectDir}/services/board/api/readmodel")
 project(":board:exception").projectDir = file("${rootProject.projectDir}/services/board/api/exception")
+project(":board:application").projectDir = file("${rootProject.projectDir}/services/board/application")
