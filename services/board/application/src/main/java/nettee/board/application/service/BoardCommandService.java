bebaa8f324e3dc77bdafa3d8f03564467184fa98
merge-simpson <letsdev@kakao.com>
2025-03-17 16:51:39 +0900

    feat(board): add command service

---
+++ services/board/application/src/main/java/nettee/board/application/service/BoardCommandService.java
@@ -0,0 +1,19 @@
+package nettee.board.application.service;
+
+import lombok.RequiredArgsConstructor;
+import nettee.board.application.port.BoardCommandRepositoryPort;
+import nettee.board.application.usecase.BoardCreateUseCase;
+import nettee.board.domain.Board;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BoardCommandService implements BoardCreateUseCase {
+
+    private final BoardCommandRepositoryPort repository;
+
+    @Override
+    public Board createBoard(Board board) {
+        return repository.create(board);
+    }
+}
