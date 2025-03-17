892ff76b83f17c077ae6d57a7e97fa453d1d6dce
merge-simpson <letsdev@kakao.com>
2025-03-18 00:16:42 +0900

    refactor(board): rename commandPort::create -> commandPort::save

--- services/board/application/src/main/java/nettee/board/application/port/BoardCommandRepositoryPort.java
+++ services/board/application/src/main/java/nettee/board/application/port/BoardCommandRepositoryPort.java
@@ -3,5 +3,5 @@ package nettee.board.application.port;
 import nettee.board.domain.Board;
 
 public interface BoardCommandRepositoryPort {
-    Board create(Board board);
+    Board save(Board board);
 }
