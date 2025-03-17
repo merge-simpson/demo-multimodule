42b8e61dab792ef891a418fc1fe58a03f90720fd
merge-simpson <letsdev@kakao.com>
2025-03-18 04:53:26 +0900

    build(board): integrate :nettee:jpa-core to :board:rdb-adapter

--- services/board/driven/rdb/build.gradle.kts
+++ services/board/driven/rdb/build.gradle.kts
@@ -7,6 +7,7 @@ java {
 dependencies {
     api(project(":board:api"))
     api(project(":board:application"))
+    api(project(":nettee:jpa-core"))
 
     // if flyway configuration required
     compileOnly("org.flywaydb:flyway-database-postgresql")
