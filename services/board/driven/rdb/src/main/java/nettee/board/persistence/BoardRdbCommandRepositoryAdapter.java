987880df69080d3cafeb92d0aa0282cb2847a7c3
merge-simpson <letsdev@kakao.com>
2025-03-18 05:17:14 +0900

    refactor(board): rename `BoardJpaRepository` to `BoardCommandJpaRepository`

--- services/board/driven/rdb/src/main/java/nettee/board/persistence/BoardRdbCommandRepositoryAdapter.java
+++ services/board/driven/rdb/src/main/java/nettee/board/persistence/BoardRdbCommandRepositoryAdapter.java
@@ -4,19 +4,19 @@ import lombok.RequiredArgsConstructor;
 import nettee.board.application.port.BoardCommandRepositoryPort;
 import nettee.board.domain.Board;
 import nettee.board.persistence.mapper.BoardEntityMapper;
-import nettee.board.persistence.repository.BoardJpaRepository;
+import nettee.board.persistence.repository.BoardCommandJpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
 public class BoardRdbCommandRepositoryAdapter implements BoardCommandRepositoryPort {
 
-    private final BoardJpaRepository boardJpaRepository;
+    private final BoardCommandJpaRepository commandJpaRepository;
     private final BoardEntityMapper mapper;
 
     @Override
     public Board save(Board board) {
         var entity = mapper.toEntity(board);
-        return mapper.toDomain(boardJpaRepository.save(entity));
+        return mapper.toDomain(commandJpaRepository.save(entity));
     }
 }
--- services/board/driven/rdb/src/main/java/nettee/board/persistence/repository/BoardCommandJpaRepository.java
+++ services/board/driven/rdb/src/main/java/nettee/board/persistence/repository/BoardCommandJpaRepository.java
@@ -3,6 +3,6 @@ package nettee.board.persistence.repository;
 import nettee.board.persistence.entity.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BoardJpaRepository extends JpaRepository<BoardEntity, Long> {
+public interface BoardCommandJpaRepository extends JpaRepository<BoardEntity, Long> {
 
 }
