997e11acb0e0ea609bcec447600a549a4cacbb20
merge-simpson <letsdev@kakao.com>
2025-03-18 13:22:19 +0900

    build(board): add jakarta validation api to web adapter using compileOnly

--- services/board/driving/web/build.gradle.kts
+++ services/board/driving/web/build.gradle.kts
@@ -8,6 +8,9 @@ dependencies {
     api(project(":board:api"))
     api(project(":board:application"))
 
+    // validation annotation
+    compileOnly("jakarta.validation:jakarta.validation-api")
+
     // mapstruct
     compileOnly("org.mapstruct:mapstruct:1.6.3")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
