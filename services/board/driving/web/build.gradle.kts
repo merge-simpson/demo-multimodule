2bb0097093677c5e1b8bdc16d8fbec39003de7db
merge-simpson <letsdev@kakao.com>
2025-03-18 13:21:50 +0900

    build(board): add mapstruct annotation processor and lombok-mapstruct-binding to web adapter

--- services/board/driving/web/build.gradle.kts
+++ services/board/driving/web/build.gradle.kts
@@ -10,4 +10,6 @@ dependencies {
 
     // mapstruct
     compileOnly("org.mapstruct:mapstruct:1.6.3")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
+    annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
 }
