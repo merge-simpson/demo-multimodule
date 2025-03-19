f9449c3ac6cbce9797267623b37a7ff0f7d54666
merge-simpson <letsdev@kakao.com>
2025-03-19 18:38:41 +0900

    build(board): integrate locale-webmvc to web adapter

--- services/board/driving/web/build.gradle.kts
+++ services/board/driving/web/build.gradle.kts
@@ -7,6 +7,7 @@ java {
 dependencies {
     api(project(":board:api"))
     api(project(":board:application"))
+    api(project(":nettee:locale-webmvc"))
 
     // validation annotation
     compileOnly("jakarta.validation:jakarta.validation-api")
