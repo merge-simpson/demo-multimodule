c4407e6b0ed68013f4e64ef40c101d913896e6dd
merge-simpson <letsdev@kakao.com>
2025-03-19 18:24:23 +0900

    build(locale webmvc): add required dependencies

--- core-modules/i18n/locale-webmvc/build.gradle.kts
+++ core-modules/i18n/locale-webmvc/build.gradle.kts
@@ -5,5 +5,7 @@ java {
 }
 
 dependencies {
-
+    compileOnly("org.springframework:spring-webmvc")
+    compileOnly("jakarta.annotation:jakarta.annotation-api:2.1.1")
+    compileOnly("jakarta.servlet:jakarta.servlet-api")
 }
