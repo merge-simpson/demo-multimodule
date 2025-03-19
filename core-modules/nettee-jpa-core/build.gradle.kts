b75e0cf9aeeffe9556aa852abae6467ec4188484
merge-simpson <letsdev@kakao.com>
2025-03-19 16:49:49 +0900

    build(jpa): remove the unnecessary repositories, tasks.test

--- core-modules/nettee-jpa-core/build.gradle.kts
+++ core-modules/nettee-jpa-core/build.gradle.kts
@@ -4,17 +4,9 @@ java {
     }
 }
 
-repositories {
-    mavenCentral()
-}
-
 dependencies {
     compileOnly("jakarta.persistence:jakarta.persistence-api")
     compileOnly("org.springframework.data:spring-data-commons")
     compileOnly("org.springframework.data:spring-data-jpa")
     compileOnly("org.springframework.boot:spring-boot-autoconfigure")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
