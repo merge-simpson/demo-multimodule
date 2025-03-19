9f1635627d629a5ec0ba9708a3e96d029ef3be49
merge-simpson <letsdev@kakao.com>
2025-03-19 18:23:23 +0900

    build(locale webmvc): register the locale webmvc module

--- gradle-scripts/core.gradle.kts
+++ gradle-scripts/core.gradle.kts
@@ -5,6 +5,7 @@ include(
     ":nettee:cors-webmvc",
     ":nettee:cors-api",
     ":nettee:exception-handler-webmvc",
+    ":nettee:locale-webmvc",
 )
 
 project(":nettee:jpa-core").projectDir = file("$coreDir/nettee-jpa-core")
@@ -12,3 +13,4 @@ project(":nettee:cors-webmvc").projectDir = file("$coreDir/cors/nettee-cors-webm
 project(":nettee:cors-api").projectDir = file("$coreDir/cors/nettee-cors-api")
 project(":nettee:exception-handler-webmvc").projectDir =
     file("$coreDir/exception-handler/nettee-global-exception-handler-webmvc")
+project(":nettee:locale-webmvc").projectDir = file("$coreDir/i18n/locale-webmvc")
